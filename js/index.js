7e153d1a0fe64d12a3e35f4e5e2b36bcf7081758
Alina <hrusinskaalinka@gmail.com>
2024-09-04 15:50:36 +0300

    dz

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let age = 15;
+console.log(age);
+const name = "Alina";
+console.log(name);
+let isStudent = "false";
+console.log(isStudent);
+const myString =
+  "Про що б ви не мріяли — робіть. Бо в зухвалості є геній, сила і магія";
+console.log(myString);
+let myNumber = 11 + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+let ages = prompt("Введіть ваше ім'я");
+alert(`Вітаю ${ages}`);
+console.log(typeof ages);
+
+let confirmation = confirm("Підтвердіть вашу дію");
+console.log(confirmation);
+alert(confirmation);
